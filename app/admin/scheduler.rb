59740dcee2040385ba850b62143f301cc50aab5f
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2017-12-23 14:48:10 -0500

    Add Scheduler

--- app/admin/scheduler.rb
+++ app/admin/scheduler.rb
@@ -1,3 +1,44 @@
 ActiveAdmin.register Scheduler do
- menu priority: 3
+  require 'sidekiq'
+  require 'sidekiq/scheduler'
+
+  menu priority: 3
+
+  action_item only: %i[index show] do
+    link_to 'Clear Cache', clear_cache_admin_schedulers_path
+  end
+
+  action_item only: %i[index show] do
+    link_to 'Re-Schedule', reschedule_admin_schedulers_path
+  end
+
+  # Allows admins to clear rails cache
+  collection_action :clear_cache, method: :get do
+    Rails.cache.clear
+    redirect_to admin_schedulers_path, notice: 'Cache cleared!'
+  end
+
+  # Allows admins to Reschedule tasks
+  collection_action :reschedule, method: :get do
+    Sidekiq.schedule = Scheduler.all.map do |scheduler|
+      {
+        scheduler.id => {
+          cron: scheduler.process_time,
+          class: RunQuery,
+          args: scheduler.process_statement,
+          description: "Run Query #{scheduler.process_statement} at #{scheduler.process_time}"
+        }
+      }
+    end
+    Sidekiq::Scheduler.reload_schedule!
+    redirect_to admin_schedulers_path, notice: 'Rescheduled!'
+  end
 end
+
+
+# clear_leaderboards_contributors:
+#   cron: '0 30 6 * * 1'
+#   class: ClearLeaderboards
+#   queue: low
+#   args: contributors
+#   description: 'This job resets the weekly leaderboard for contributions'
---
+++ app/workers/RunQuery.rb
@@ -0,0 +1,10 @@
+require 'sidekiq-scheduler'
+
+class RunQuery
+  include Sidekiq::Worker
+
+  def perform(query)
+    connection = ActiveRecord::Base.connection
+    connection.execute(query)
+  end
+end
