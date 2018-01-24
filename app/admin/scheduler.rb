209764b426b7a277e4318977fd059837b8eb025f
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2018-01-24 08:21:20 -0500

    Add run now to Google Analytics to CMS

--- app/admin/scheduler.rb
+++ app/admin/scheduler.rb
@@ -21,30 +21,18 @@ ActiveAdmin.register Scheduler do
   collection_action :reschedule, method: :get do
     Sidekiq::Cron::Job.destroy_all!
     Scheduler.where(active: true).map do |scheduler|
-      if scheduler.worker == 'Query'
-        Sidekiq::Cron::Job.create(
-          name: "Schedule: #{scheduler.id}",
-          cron: scheduler.process_time,
-          class: 'RunQuery',
-          args: [scheduler.process_statement]
-        )
-      elsif scheduler.worker == 'Appboy'
-        Sidekiq::Cron::Job.create(
-          name: "Schedule: #{scheduler.id}",
-          cron: scheduler.process_time,
-          class: 'RunAppboy',
-          args: [JSON.parse(scheduler.process_statement)]
-        )
-      elsif scheduler.worker == 'Google Analytics'
-        Sidekiq::Cron::Job.create(
-          name: "Schedule: #{scheduler.id}",
-          cron: scheduler.process_time,
-          class: 'RunGoogleAnalytics',
-          args: [JSON.parse(scheduler.process_statement)]
-        )
-      end
+      schedule_job(scheduler)
     end
     redirect_to admin_schedulers_path, notice: 'Rescheduled!'
+  end
+
+  action_item :run_now, only: :show, :edit do
+     link_to 'Run now', run_now_admin_scheduler_path(membership)
+  end
+  # Allows admins to run tasks
+  member_action :run_now, method: :get do
+    run_now(Scheduler.find(params[:id]))
+    redirect_to admin_schedulers_path, notice: 'Executing!'
   end
 
   form do |f|
@@ -71,5 +59,45 @@ ActiveAdmin.register Scheduler do
     column :updated_at
 
     actions
+  end
+
+  controller do
+    # This code is evaluated within the controller class
+
+    def schedule_job(scheduler)
+      if scheduler.worker == 'Query'
+        Sidekiq::Cron::Job.create(
+          name: "Schedule: #{scheduler.id}",
+          cron: scheduler.process_time,
+          class: 'RunQuery',
+          args: [scheduler.process_statement]
+        )
+      elsif scheduler.worker == 'Appboy'
+        Sidekiq::Cron::Job.create(
+          name: "Schedule: #{scheduler.id}",
+          cron: scheduler.process_time,
+          class: 'RunAppboy',
+          args: [JSON.parse(scheduler.process_statement)]
+        )
+      elsif scheduler.worker == 'Google Analytics'
+        Sidekiq::Cron::Job.create(
+          name: "Schedule: #{scheduler.id}",
+          cron: scheduler.process_time,
+          class: 'RunGoogleAnalytics',
+          args: [JSON.parse(scheduler.process_statement)]
+        )
+      end
+    end
+
+    def run_now(scheduler)
+      if scheduler.worker == 'Query'
+        RunQuery.new.perform([scheduler.process_statement])
+      elsif scheduler.worker == 'Appboy'
+        RunAppboy.new.perform([JSON.parse(scheduler.process_statement)])
+      elsif scheduler.worker == 'Google Analytics'
+        RunGoogleAnalytics.new.perform([JSON.parse(scheduler.process_statement)])
+      end
+    end
+
   end
 end
