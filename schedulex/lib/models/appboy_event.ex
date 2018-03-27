89ba3ee47cd673804d32120f8f9eaf0bcc2af26c
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2018-03-27 16:15:18 -0400

    Update model data for appboy event

--- schedulex/lib/models/appboy_event.ex
+++ schedulex/lib/models/appboy_event.ex
@@ -1,9 +1,9 @@
-defmodule Schedulex.Models.AppboyAttribute do
+defmodule Schedulex.Models.AppboyEvent do
   use Ecto.Schema
   alias Schedulex.Repo
-  alias Schedulex.Models.AppboyAttribute
+  alias Schedulex.Models.AppboyEvent
   import Ecto.Query, only: [from: 2]
-  schema "appboy_attributes" do
+  schema "appboy_events" do
     field :user_id, :integer
     field :event_name, :string
     field :status, :string
@@ -14,12 +14,12 @@ defmodule Schedulex.Models.AppboyAttribute do
   Select clause
   Examples
 
-      iex> Schedulex.Models.AppboyAttribute.users_in(table_name, ids)
-      %Schedulex.Models.AppboyAttribute{}
+      iex> Schedulex.Models.AppboyEvent.users_in(table_name, ids)
+      %Schedulex.Models.AppboyEvent{}
   """
   def users_in(tbl, ids) do
     from(
-      p in {tbl, AppboyAttribute},
+      p in {tbl, AppboyEvent},
       where: p.id in ^ids
     ) |> Repo.all
   end
@@ -28,12 +28,12 @@ defmodule Schedulex.Models.AppboyAttribute do
   Update status
   Examples
 
-      iex> Schedulex.Models.AppboyAttribute.users_in(table_name, ids)
-      %Schedulex.Models.AppboyAttribute{}
+      iex> Schedulex.Models.AppboyEvent.users_in(table_name, ids)
+      %Schedulex.Models.AppboyEvent{}
   """
   def set_status_for_users_in(tbl, ids, status) do
     from(
-      p in {tbl, AppboyAttribute},
+      p in {tbl, AppboyEvent},
       where: p.id in ^ids,
       update: [set: [status: ^status, date_sent: ^NaiveDateTime.utc_now]]
     ) |> Schedulex.Repo.update_all([])
@@ -43,13 +43,13 @@ defmodule Schedulex.Models.AppboyAttribute do
   Fetch in batches
   Examples
 
-      iex> Schedulex.Models.AppboyAttribute.users_in(table_name, ids)
-      %Schedulex.Models.AppboyAttribute{}
+      iex> Schedulex.Models.AppboyEvent.users_in(table_name, ids)
+      %Schedulex.Models.AppboyEvent{}
   """
   @batch_size 50
   def batch_unprocessed_users(tbl) do
     from(
-      u in {tbl, AppboyAttribute},
+      u in {tbl, AppboyEvent},
       select: u.id,
       where: is_nil(u.status) and not is_nil(u.user_id) and not is_nil(u.event_name),
       limit: @batch_size
